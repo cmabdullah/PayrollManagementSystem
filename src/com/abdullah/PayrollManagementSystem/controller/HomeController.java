4e639262041ff18b3c8ba840d7ceb11bf555e066
cmabdullah <cmabdullah21@gmail.com>
2018-11-02 22:06:25 +0600

    Log4j implementation add

--- src/com/abdullah/PayrollManagementSystem/controller/HomeController.java
+++ src/com/abdullah/PayrollManagementSystem/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.abdullah.PayrollManagementSystem.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
-	
+	private static Logger logger = Logger.getLogger(HomeController.class);
 	@RequestMapping("/")
 	public String showHome() {
+		logger.info("Showing home.....");
 		return "home";
 	}
 	
