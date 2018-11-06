1404458a26c35c83d668dc5c410c968da14f1034
cmabdullah <cmabdullah21@gmail.com>
2018-11-06 23:30:24 +0600

    There no  :bug: find, improve code quality

--- src/com/abdullah/PayrollManagementSystem/controller/LoanController.java
+++ src/com/abdullah/PayrollManagementSystem/controller/LoanController.java
@@ -1,11 +1,16 @@
 package com.abdullah.PayrollManagementSystem.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.abdullah.PayrollManagementSystem.dao.Loan;
 @Controller
@@ -17,10 +22,22 @@ public class LoanController {
 		logger.info("Showing loan.....");
 		return "loanreq";
 	}
-	
-	@RequestMapping("/loanreq_process")
-	public String requestForLoanProcess(Model model, @Valid Loan loan) {
+	//@RequestMapping("/loanreq_process")
+	@RequestMapping(value = "/loanreq_process", method=RequestMethod.POST)
+	public String requestForLoanProcess(Model model, @Valid Loan loan , BindingResult result) {
 		logger.info("Showing loan....."+loan);
+		
+		
+		if (result.hasErrors()) {
+			List<ObjectError> errors = result.getAllErrors();
+			for (ObjectError error: errors) {
+				System.out.println(error.getDefaultMessage());
+			}
+		}else {
+			System.out.println("Form is validet");
+		}
+		
+		
 		return "loanreq";
 	}
 	
