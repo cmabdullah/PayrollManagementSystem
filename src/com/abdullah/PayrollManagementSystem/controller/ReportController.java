3c0411b321c0b17e1df50216e2226d368c54c520
cmabdullah <cmabdullah21@gmail.com>
2018-12-09 16:38:41 +0600

    Done done Report generate complete

--- src/com/abdullah/PayrollManagementSystem/controller/ReportController.java
+++ src/com/abdullah/PayrollManagementSystem/controller/ReportController.java
@@ -9,18 +9,39 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
-
+//defining beans [PdfRevenueSummary]
 @Controller
-public class ReportController extends AbstractController{
+public class ReportController extends AbstractController {
 	private static Logger logger = Logger.getLogger(ReportController.class);
 
-	@RequestMapping("/report")
-	public String fetchReport(Model model) {
-		logger.info("Showing report.....");
+//	@RequestMapping("/report")
+//	public String fetchReport(Model model) {
+//		logger.info("Showing report.....");
+//
+//		// dummy data
+//		Map<String, String> revenueData = new HashMap<String, String>();
+//		revenueData.put("1/20/2010", "$100,000");
+//		revenueData.put("1/21/2010", "$200,000");
+//		revenueData.put("1/22/2010", "$300,000");
+//		revenueData.put("1/23/2010", "$400,000");
+//		revenueData.put("1/24/2010", "$500,000");
+//
+//		model.addAttribute("revenueData", revenueData);
+//
+//		return "report";
+//	}
 
+	@RequestMapping("/report")
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+//		String output =
+//				ServletRequestUtils.getStringParameter(request, "output");
+			
 		// dummy data
 		Map<String, String> revenueData = new HashMap<String, String>();
 		revenueData.put("1/20/2010", "$100,000");
@@ -29,16 +50,24 @@ public class ReportController extends AbstractController{
 		revenueData.put("1/23/2010", "$400,000");
 		revenueData.put("1/24/2010", "$500,000");
 		
-		model.addAttribute("revenueData",revenueData);
+		return new ModelAndView("PdfRevenueSummary", "revenueData", revenueData);
 
-		return "report";
-	}
-
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		//return new ModelAndView("report", "revenueData", revenueData);
+		
+//		if (output == null || "".equals(output)) {
+//			// return normal view
+//			return new ModelAndView("RevenueSummary", "revenueData", revenueData);
+//
+//		} else if ("PDF".equals(output.toUpperCase())) {
+//			// return excel view
+//			return new ModelAndView("PdfRevenueSummary", "revenueData", revenueData);
+//
+//		} else {
+//			// return normal view
+//			return new ModelAndView("RevenueSummary", "revenueData", revenueData);
+//
+//		}
+		
 	}
 
 }
