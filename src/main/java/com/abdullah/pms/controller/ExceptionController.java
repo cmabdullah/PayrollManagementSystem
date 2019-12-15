ba632c511dbe86a0965dc0036e6f3037fb03ce18
cmabdullah <cmabdullah21@gmail.com>
2019-12-15 22:28:32 +0600

    exception handaler add

---
+++ src/main/java/com/abdullah/pms/controller/ExceptionController.java
@@ -0,0 +1,24 @@
+package com.abdullah.pms.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@Controller("error")
+public class ExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+        log.error("Request: " + req.getRequestURL() + " raised " + ex);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", ex);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error");
+        return mav;
+    }
+}
